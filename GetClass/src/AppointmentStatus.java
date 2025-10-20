fd2494e041296e3bba0a5421416f8e1bae457214
Jhonudistri <jjgonzalezc@udistrital.edu.co>
2025-10-20 10:52:10 -0500

    Update AppointmentStatus.java

--- GetClass/src/AppointmentStatus.java
+++ GetClass/src/AppointmentStatus.java
@@ -1,4 +1,4 @@
-// Enumeration to define the possible states of an appointment
+// Enumeration to define the possible states of an appointment.
 public enum AppointmentStatus {
     PENDING, // apppoinment in mode pendind  to accept
     ACCEPTED, // accept appointmet
